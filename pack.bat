ab412faaf618c61b2a500f733e4cf9dd2072fd6e
SongDoublerain <835367568@qq.com>
2014-05-19 23:11:01 +0800

    v1.1
    调整程序注释

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 用来打包用的批处理程序，免去在cmd操作
+echo 用来打包用的批处理，免去在cmd操作
 echo 正在打包
 python pack.py py2exe
 echo 打包完成
--- pack.py
+++ pack.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-'''这只个打包用的代码，会被pack.bat文件调用，目的是把Qbao.py打包成exe文件，
+'''这只是个打包用的代码，会被pack.bat文件调用，目的是把Qbao.py打包成exe文件，
 供没装python的使用，打包用的库叫 py2exe'''
 from distutils.core import setup
 import py2exe
